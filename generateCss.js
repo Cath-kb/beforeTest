ed9d194da80ca13b5f41b64bc907a707ea44ab76
Cath_kb <catherine.nikolaev@gmail.com>
2015-09-25 09:08:54 +0300

    html and css temptates improvement

--- generateCss.js
+++ generateCss.js
@@ -1,52 +1,53 @@
+var count = 100;
 var fs = require('fs');
 
 var header = '<!doctype html>\n\
 <html>\n\
 <head>\n\
-<title>indexBefore</title>\n\
-<link rel="stylesheet" href="style.css" type="text/css" media="screen" />\n\
+<title>test before</title>\n\
+<link rel="stylesheet" href="styleBefore.css" type="text/css" media="screen" />\n\
 </head>\n\
 <body>\n\
 ';
 
-var block = '\
-    <div>block content</div>\n\
-';
+var block_start = '<div class="div';
+var block_end = '">block content</div>\n';
 
 var footer = '\
 </body>\n\
 </html>\n\
 ';
 
-var cssRule = 'div:before {\n\
-content: "inner content ";\n\
-}\n\
-';
+var cssRule_start = '.div';
 
-var count = 10000;
+var cssRule_end = ':before {\n\
+	content: "inner content ";\n\
+	color: #f00;\n\
+	}\n';
+
 
 //generate html file
-var html_file_name_1 = 'indexBefore.html';
-var html_file_descriptor = fs.openSync(html_file_name_1, 'w');
+var html_file_name = 'indexBefore.html';
+var html_file_descriptor = fs.openSync(html_file_name, 'w');
 
 fs.writeSync(html_file_descriptor, header);
 for (var i = 0; i < count; ++i) {
-	fs.writeSync(html_file_descriptor, block);
+	fs.writeSync(html_file_descriptor, block_start+i+block_end);
 }
 fs.writeSync(html_file_descriptor, footer);
 
 fs.closeSync(html_file_descriptor);
 
-console.log('Done generate ' + html_file_name_1);
+console.log('Done generate ' + html_file_name);
 
 //generate css file
-var css_file_name_1 = 'style.css';
-var css_file_descriptor = fs.openSync(css_file_name_1, 'w');
+var css_file_name = 'styleBefore.css';
+var css_file_descriptor = fs.openSync(css_file_name, 'w');
 
 for (var j = 0; j < count; ++j) {
-	fs.writeSync(css_file_descriptor, cssRule);
+	fs.writeSync(css_file_descriptor, cssRule_start+j+cssRule_end);
 }
 
 fs.closeSync(css_file_descriptor);
 
-console.log('Done generate ' + css_file_name_1);
+console.log('Done generate ' + css_file_name);
--- generateHtml.js
+++ generateHtml.js
@@ -1,30 +1,49 @@
+var count = 100;
 var fs = require('fs');
 
-var header = '<!doctype html>\n\
+var header = '<!DOCTYPE html>\n\
 <html>\n\
+<head>\n\
+<title>test span</title>\n\
+<link rel="stylesheet" href="styleSpan.css" type="text/css" media="screen" />\n\
+</head>\n\
 <body>\n\
 ';
 
-var block = '\
-    <div><span>inner content </span>block content</div>\n\
-';
+var block_start = '<div class="div';
+var block_end = '"><span>inner content </span>block content</div>\n';
 
 var footer = '\
 </body>\n\
 </html>\n\
 ';
 
-var count = 10000;
+var cssRule_start = '.div';
 
-var html_file_name_1 = 'indexSpan.html';
-var html_file_descriptor = fs.openSync(html_file_name_1, 'w');
+var cssRule_end = ' span {\ncolor: #f00;\n}\n';
+
+
+var html_file_name = 'indexSpan.html';
+var html_file_descriptor = fs.openSync(html_file_name, 'w');
 
 fs.writeSync(html_file_descriptor, header);
 for (var i = 0; i < count; ++i) {
-	fs.writeSync(html_file_descriptor, block);
+	fs.writeSync(html_file_descriptor, block_start+i+block_end);
 }
 fs.writeSync(html_file_descriptor, footer);
 
 fs.closeSync(html_file_descriptor);
 
-console.log('Done generate ' + html_file_name_1);
+console.log('Done generate ' + html_file_name);
+
+//generate css file
+var css_file_name = 'styleSpan.css';
+var css_file_descriptor = fs.openSync(css_file_name, 'w');
+
+for (var j = 0; j < count; ++j) {
+	fs.writeSync(css_file_descriptor, cssRule_start+j+cssRule_end);
+}
+
+fs.closeSync(css_file_descriptor);
+
+console.log('Done generate ' + css_file_name);
